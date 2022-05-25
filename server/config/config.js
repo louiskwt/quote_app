890acb148c56d57f623666a5591fa07e8ffe9b6c
Louis Tsang <kawingt2@gmail.com>
2022-05-25 23:13:35 +0800

    connected to postgres DB

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,10 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
+    "username": "postgres",
+    "password": process.env.DB_PASSWORD,
+    "database": "quotes",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
