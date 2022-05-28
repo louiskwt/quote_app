26ac8ba671738175af3ddd58813ffb1afa1fc380
Louis Tsang <kawingt2@gmail.com>
2022-05-28 23:45:22 +0800

    fixed json array insert porblem

---
+++ server/seeders/20220528135206-demo-quotes.js
@@ -0,0 +1,65 @@
+'use strict';
+
+function cleanEntry(obj) {
+  return JSON.stringify(obj).replace(/"/g, '\\"')
+}
+
+function cleanArray(arr) {
+  return '{"' + arr.map(e => cleanEntry(e)).join('", "') + '"}'
+}
+
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     */
+    await queryInterface.bulkInsert('quotes', [
+      {
+        name: '馮小姐',
+        address: "九龍何文田半山一號10樓J",
+        contents: cleanArray([
+          {
+            "item": "清拆全屋傢俬",
+            "price": "28000",
+          },
+          {
+            "item": "更換全屋電器",
+            "price": "28000",
+          }
+        ]),
+        memo: ["工程可能因為天氣而延遲10天"],
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "李生",
+        address: "西半山好景花園一期D座8樓A",
+        contents: cleanArray([
+          {
+            "item": "清拆全屋傢俬",
+            "price": "28000",
+          },
+          {
+            "item": "更換全屋電器",
+            "price": "28000",
+          }
+        ]),
+        memo: ["工程可能因為天氣而延遲10天$開工後先付30%$完工後付70%"],
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+  ], {
+    contents: { type: new Sequelize.JSON() }
+  });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
