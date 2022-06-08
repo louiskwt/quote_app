cdf2bc5325d385106c503022242de7827918116d
Louis Tsang <kawingt2@gmail.com>
2022-06-08 22:38:26 +0800

    add toast to form operations

--- client/src/components/QuoteCard/QuoteCard.jsx
+++ client/src/components/QuoteCard/QuoteCard.jsx
@@ -1,60 +1,73 @@
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
-import quoteApi from '../../apis/quoteApi';
-import { useContext } from 'react';
-import { QuotesContext } from '../../context/quotesContext';
-import { UserContext } from '../../context/userContext'
+import quoteApi from "../../apis/quoteApi";
+import { useContext } from "react";
+import { QuotesContext } from "../../context/quotesContext";
+import { UserContext } from "../../context/userContext";
+import { toast } from "react-toastify";
 
+const QuoteCard = ({ quote }) => {
+  const { quotes, setQuotes } = useContext(QuotesContext);
+  const { userState } = useContext(UserContext);
 
-
-const QuoteCard = ({quote}) => {
-
-  const {quotes, setQuotes } = useContext(QuotesContext)
-  const {userState} = useContext(UserContext);
-  
   const handleDelete = async (id) => {
     try {
       await quoteApi.delete(`/${id}`, {
-          headers: {"x-access-token": userState.token}
-        });
+        headers: { "x-access-token": userState.token },
+      });
       setQuotes(quotes.filter((quote) => quote.id !== id));
+      toast.success("已刪除報價單");
     } catch (error) {
-      alert(error);
+      toast.error(`刪除報價單失敗：${error.message}`);
     }
-  }
+  };
 
   return (
-    <Card sx={{ width: '350px', textAlign: 'left', marginTop: '1rem' }} className="quote-card" data-cy="quote-card">
-        <CardContent>
-              <Typography variant="h6" component="div">
-                  {quote.address}
-              </Typography>
-              <Typography variant="body1" sx={{ mt: 3 }}>
-                  客戶: {quote.name}
-                  <br/>
-                  日期: {new Date(quote.updatedAt).toLocaleDateString()}
-              </Typography>
-        </CardContent>
-        <CardActions sx={{ p: 2 }}>
-          <Button size="small" sx={{ fontWeight: 800 }} variant="outlined">
-            <Link to={`/${quote.id}`} style={{ listStyle: 'none', textDecoration: 'none', color: 'inherit', fontSize: '15px'}} >
-                查看
-             </Link>
-          </Button>
-  
-            <Button sx={{ ml: 3, fontWeight: 800, }} color='error' variant="outlined" onClick={() => handleDelete(quote.id)} >
-              <span >
-                  刪除
-              </span>
-            </Button>
-    
-         </CardActions>
-    </Card>
-  )
-}
+    <Card
+      sx={{ width: "350px", textAlign: "left", marginTop: "1rem" }}
+      className="quote-card"
+      data-cy="quote-card"
+    >
+      <CardContent>
+        <Typography variant="h6" component="div">
+          {quote.address}
+        </Typography>
+        <Typography variant="body1" sx={{ mt: 3 }}>
+          客戶: {quote.name}
+          <br />
+          日期: {new Date(quote.updatedAt).toLocaleDateString()}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ p: 2 }}>
+        <Button size="small" sx={{ fontWeight: 800 }} variant="outlined">
+          <Link
+            to={`/${quote.id}`}
+            style={{
+              listStyle: "none",
+              textDecoration: "none",
+              color: "inherit",
+              fontSize: "15px",
+            }}
+          >
+            查看
+          </Link>
+        </Button>
 
-export default QuoteCard
+        <Button
+          sx={{ ml: 3, fontWeight: 800 }}
+          color="error"
+          variant="outlined"
+          onClick={() => handleDelete(quote.id)}
+        >
+          <span>刪除</span>
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default QuoteCard;
--- client/src/context/formsContext.jsx
+++ client/src/context/formsContext.jsx
@@ -1,160 +1,161 @@
-import { createContext, useReducer } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { v4 as uuidv4} from 'uuid';
-import quoteApi from '../apis/quoteApi';
-
+import { createContext, useReducer } from "react";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
+import quoteApi from "../apis/quoteApi";
+import { toast } from "react-toastify";
 
 const FormsContext = createContext();
 
 // initial form state
 const initialFormState = {
-    id: '',
-    name: '',
-    address: '',
-    contents: [
-        {
-            id: uuidv4(),
-            item: '',
-            price: '',
-        }
-    ],
-    memo: ''
-}
+  id: "",
+  name: "",
+  address: "",
+  contents: [
+    {
+      id: uuidv4(),
+      item: "",
+      price: "",
+    },
+  ],
+  memo: "",
+};
 
 const formReducer = (formState, formAction) => {
-    switch (formAction.type) {
-        case "SET_GENERAL_INPUT":
-            return { ...formState, [formAction.field]: formAction.value }
-        case "ADD_CONTENT":
-            return { ...formState, contents: formAction.updatedContents};
-        case "DELETE_CONTENT":
-            return {...formState, contents: formState.contents.filter((content) => content.id !== formAction.id)};
-        case "SET_CONTENT_INPUT":
-            return { ...formState, contents: formAction.updatedContents};
-        case "SET_UPDATE_FORM":
-            return {
-                id: formAction.quote.id,
-                name: formAction.quote.name,
-                address: formAction.quote.address,
-                contents: formAction.quote.contents,
-                memo: formAction.quote.memo.join('$'),
-            };
-        case "RESET_FORM_STATE":
-            return {
-                ...initialFormState
-            };
-        default:
-            return formState;
+  switch (formAction.type) {
+    case "SET_GENERAL_INPUT":
+      return { ...formState, [formAction.field]: formAction.value };
+    case "ADD_CONTENT":
+      return { ...formState, contents: formAction.updatedContents };
+    case "DELETE_CONTENT":
+      return {
+        ...formState,
+        contents: formState.contents.filter(
+          (content) => content.id !== formAction.id
+        ),
+      };
+    case "SET_CONTENT_INPUT":
+      return { ...formState, contents: formAction.updatedContents };
+    case "SET_UPDATE_FORM":
+      return {
+        id: formAction.quote.id,
+        name: formAction.quote.name,
+        address: formAction.quote.address,
+        contents: formAction.quote.contents,
+        memo: formAction.quote.memo.join("$"),
+      };
+    case "RESET_FORM_STATE":
+      return {
+        ...initialFormState,
+      };
+    default:
+      return formState;
+  }
+};
+
+const FormsContextProvider = ({ children }) => {
+  let navigate = useNavigate();
+
+  const [formState, dispatchForm] = useReducer(formReducer, initialFormState);
+
+  const handleGeneralInput = (field, value) => {
+    dispatchForm({ type: "SET_GENERAL_INPUT", field, value });
+  };
+
+  const setUpdateForm = (quote) => {
+    dispatchForm({ type: "SET_UPDATE_FORM", quote });
+  };
+
+  const addContent = () => {
+    // set up an empty content
+    const content = {
+      id: uuidv4(),
+      item: "",
+      price: "",
+    };
+    const updatedContents = [...formState.contents, content];
+    dispatchForm({ type: "ADD_CONTENT", updatedContents });
+  };
+
+  const deleteContent = (id) => {
+    // filter out the content based on id
+    dispatchForm({ type: "DELETE_CONTENT", id });
+  };
+
+  const handleContentInput = (index, key, value) => {
+    let updatedContents = [...formState.contents];
+    updatedContents[index][key] = value;
+
+    dispatchForm({ type: "SET_CONTENT_INPUT", updatedContents });
+  };
+
+  const handleFormSubmit = async (event, token) => {
+    let formData = {
+      name: formState.name,
+      address: formState.address,
+      contents: formState.contents,
+      memo: [formState.memo],
+    };
+    console.log(formData);
+    event.preventDefault();
+
+    try {
+      const data = await quoteApi.post("/", formData, {
+        headers: {
+          "x-access-token": token,
+        },
+      });
+      toast.success("已新增報價單");
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+      toast.error(`新增報價單失敗：${error.message}`);
     }
-}
+  };
 
-const FormsContextProvider = ({children}) => {
-    let navigate = useNavigate();
+  const handleFormUpdate = async (event, token) => {
+    let formData = {
+      name: formState.name,
+      address: formState.address,
+      contents: formState.contents,
+      memo: [formState.memo],
+    };
 
-    const [formState, dispatchForm] = useReducer(formReducer, initialFormState);
+    event.preventDefault();
 
-    const handleGeneralInput = (field, value) => {
-        dispatchForm({ type: "SET_GENERAL_INPUT", field, value })
+    try {
+      await quoteApi.put(`/${formState.id}`, formData, {
+        headers: {
+          "x-access-token": token,
+        },
+      });
+      toast.success("已更新報價單");
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+      toast.error(`更生報價單失敗：${error.message}`);
     }
+  };
 
-    const setUpdateForm = (quote) => {
-        dispatchForm({ type: "SET_UPDATE_FORM", quote });
-    }
+  const resetForm = () => {
+    dispatchForm({ type: "RESET_FORM_STATE" });
+  };
 
-    const addContent = () => {
-        // set up an empty content
-        const content = {
-            id: uuidv4(),
-            item: '',
-            price: '',
-        }
-        const updatedContents = [...formState.contents, content];
-        dispatchForm({ type: "ADD_CONTENT", updatedContents });
-    }
+  const value = {
+    handleGeneralInput,
+    addContent,
+    deleteContent,
+    handleContentInput,
+    handleFormSubmit,
+    handleFormUpdate,
+    setUpdateForm,
+    resetForm,
+    formState,
+  };
 
+  return (
+    <FormsContext.Provider value={value}>{children}</FormsContext.Provider>
+  );
+};
 
-    const deleteContent = (id) => {
-        // filter out the content based on id
-        dispatchForm({ type: "DELETE_CONTENT", id })
-    }
-
-    const handleContentInput = (index, key, value) => {
-        let updatedContents = [...formState.contents];
-        updatedContents[index][key] = value;
-
-        dispatchForm({ type: "SET_CONTENT_INPUT", updatedContents });
-    }
-
-    const handleFormSubmit = async (event, token) => {
-        let formData = {
-            name: formState.name,
-            address: formState.address,
-            contents: formState.contents,
-            memo: [formState.memo]
-        };
-        console.log(formData);
-        event.preventDefault();
-
-        try {
-            const data = await quoteApi.post('/', formData, {
-                headers: {
-                    "x-access-token": token
-                }
-            });
-            console.log(data);
-            navigate('/');
-        } catch (error) {
-            console.log(error.message);
-            alert(error.message);
-        }
-    }
-
-    const handleFormUpdate = async (event, token) => {
-        let formData = {
-            name: formState.name,
-            address: formState.address,
-            contents: formState.contents,
-            memo: [formState.memo]
-        };
-        console.log(formData);
-        event.preventDefault();
-
-        try {
-            const data = await quoteApi.put(`/${formState.id}`, formData, {
-                headers: {
-                    "x-access-token": token
-                }
-            });
-            console.log(data);
-            navigate('/');
-        } catch (error) {
-            console.log(error.message);
-            alert(error.message);
-        }
-    }
-
-    const resetForm = () => {
-        dispatchForm({type: "RESET_FORM_STATE"});
-    }
-
-
-    const value = {
-        handleGeneralInput,
-        addContent,
-        deleteContent,
-        handleContentInput,
-        handleFormSubmit,
-        handleFormUpdate,
-        setUpdateForm,
-        resetForm,
-        formState
-    }
-
-    return (
-        <FormsContext.Provider value={value}>
-            {children}
-        </FormsContext.Provider>
-    )
-}
-
-export { FormsContext, FormsContextProvider }
+export { FormsContext, FormsContextProvider };
