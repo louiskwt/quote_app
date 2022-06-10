81a7f4f1b5a92ba34e6b794610e8e0702b23bcb1
Louis Tsang <kawingt2@gmail.com>
2022-06-10 22:39:28 +0800

    fixed express urlencoded typo bug

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const { sequelize } = require('./models');
+const { sequelize } = require("./models");
 
 // bring in the routes
-const quoteRoutes = require('./routes/quoteRoutes');
-const userRoutes = require('./routes/userRoutes');
+const quoteRoutes = require("./routes/quoteRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 // create express app
 const app = express();
@@ -16,43 +16,41 @@ const app = express();
 // Middlewares
 
 // logger
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // enable cors
 app.use(cors());
 
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extend: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
 });
 
 // api routes
-app.use('/api/v1/quotes', quoteRoutes);
+app.use("/api/v1/quotes", quoteRoutes);
 
-app.use('/api/v1/users', userRoutes);
+app.use("/api/v1/users", userRoutes);
 
 const PORT = process.env.PORT;
 
-
 app.listen(PORT, () => {
-    console.log(`Server running at port: ${PORT}`);
-})
-
+  console.log(`Server running at port: ${PORT}`);
+});
 
 const db = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established');
-    } catch (error) {
-        console.log('Unable to conntect', error)
-    }
-}
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established");
+  } catch (error) {
+    console.log("Unable to conntect", error);
+  }
+};
 
 db();
