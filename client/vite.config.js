7cf5bb3e14ef9f04943970d849f7958202473957
Louis Tsang <kawingt2@gmail.com>
2022-06-13 23:19:54 +0800

    reconfigured manifest

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["src/*.png", "src/*ico"],
+      backgroundColor: "#F1DD8B",
+      themeColor: "#F1DD8B",
+      shortName: "曾氏公程公司",
+      name: "曾氏公程公司 | 報價單",
+    }),
+  ],
+});
