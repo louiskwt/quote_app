0076f9467aaadb9a18491e49ea9a4527dc637487
Louis Tsang <kawingt2@gmail.com>
2022-06-13 22:51:28 +0800

    fixed Quote Page styling

--- client/src/components/Quote/Quote.jsx
+++ client/src/components/Quote/Quote.jsx
@@ -15,13 +15,10 @@ const intlNum = new Intl.NumberFormat("en-US");
 const Item = ({ content, index }) => {
   return (
     <>
-      <Grid item xs={1} sm={1} md={1}>
-        <span>{index + 1}.</span>
+      <Grid item xs={8} fontSize="large">
+        {index + 1}. {content.item}
       </Grid>
-      <Grid item xs={7} sm={7} md={7}>
-        {content.item}
-      </Grid>
-      <Grid item xs={4} sm={4} md={4}>
+      <Grid item xs={4} fontSize="large">
         $ {intlNum.format(content.price)}
       </Grid>
     </>
@@ -31,11 +28,8 @@ const Item = ({ content, index }) => {
 const Memo = ({ memoItem, index }) => {
   return (
     <>
-      <Grid item xs={1}>
-        <span>{index + 1}.</span>
-      </Grid>
-      <Grid item xs={11}>
-        {memoItem}
+      <Grid item xs={12} fontSize="large">
+        {index + 1}. {memoItem}
       </Grid>
     </>
   );
@@ -77,22 +71,22 @@ const Quote = ({ quote }) => {
         direction={{ xs: "column" }}
         spacing={{ xs: 1, md: 2 }}
       >
-        <Typography as="span" fontWeight="bold" fontSize="1.1rem">
+        <Typography as="span" fontWeight="bold" fontSize="1.2rem">
           客戶： {quote.name}
         </Typography>
-        <Typography as="span" fontWeight="bold" fontSize="1.1rem">
+        <Typography as="span" fontWeight="bold" fontSize="1.2rem">
           地址：{quote.address}
         </Typography>
-        <Typography as="span" fontWeight="bold">
+        <Typography as="span" fontWeight="bold" fontSize="1.2rem">
           日期：{date.toLocaleDateString()}
         </Typography>
       </Stack>
       <Divider sx={{ borderColor: "#000000", mt: 3 }} />
       <Grid container rowSpacing={5} spacing={5} sx={{ mt: 2, mb: 5 }}>
-        <Grid item xs={8} sm={8}>
+        <Grid item xs={8} sm={8} fontSize="large">
           工程項目：
         </Grid>
-        <Grid item xs={4} sm={4}>
+        <Grid item xs={4} sm={4} fontSize="large">
           價錢
         </Grid>
         {quote.contents.map((content, index) => (
@@ -101,13 +95,14 @@ const Quote = ({ quote }) => {
       </Grid>
       <Divider sx={{ borderColor: "#000000", mt: 3 }} />
       <Grid container rowSpacing={3} spacing={5} sx={{ pl: 15, mt: 2, mb: 5 }}>
-        <Grid item xs={8} sm={8}></Grid>
-        <Grid item xs={4} sm={4}>
-          總數: $ {intlNum.format(totalPrice)}
+        <Grid item xs={8}></Grid>
+        <Grid item xs={4} fontSize="large" sx={{ letterSpacing: "1px" }}>
+          <Box>總數:</Box>
+          <Box mt={3}>${intlNum.format(totalPrice)}</Box>
         </Grid>
       </Grid>
       <Grid container rowSpacing={3} spacing={5} sx={{ mt: 2, mb: 5 }}>
-        <Grid item xs={12} sx={{ mt: 2 }}>
+        <Grid item xs={12} sx={{ mt: 2 }} fontSize="large">
           工程備忘：
         </Grid>
         {quote.memo &&
