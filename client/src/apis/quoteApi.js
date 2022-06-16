2251943fba2700671f9ee4d2986fac502e4fc171
Louis Tsang <kawingt2@gmail.com>
2022-06-16 21:51:46 +0800

    fixed api path

--- client/src/apis/quoteApi.js
+++ client/src/apis/quoteApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "api/v1/quotes"
+    ? "/api/v1/quotes"
     : "http://localhost:8000/api/v1/quotes";
 
 export default axios.create({
--- client/src/apis/userApi.js
+++ client/src/apis/userApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "api/v1/users"
+    ? "/api/v1/users"
     : "http://localhost:8000/api/v1/users";
 
 export default axios.create({
