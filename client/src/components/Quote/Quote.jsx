9d57823b5b315800535051441cfdb4f266ecf141
Louis Tsang <kawingt2@gmail.com>
2022-06-23 23:09:19 +0800

    fine tuned PDF

--- client/src/components/Quote/Quote.jsx
+++ client/src/components/Quote/Quote.jsx
@@ -16,11 +16,11 @@ const Item = ({ content, index }) => {
       <Grid item xs={1} sm={0.5} sx={{ mr: 0 }} fontSize="large">
         {index + 1}
       </Grid>
-      <Grid item xs={7} sm={7.5} sx={{ ml: 0 }} fontSize="large">
+      <Grid item xs={8} sm={7.5} sx={{ ml: 0 }} fontSize="large">
         {content.item}
       </Grid>
       <Grid item xs={3} sm={4} sx={{ ml: 0 }} fontSize="large">
-        $ {intlNum.format(content.price)}
+        ${intlNum.format(content.price)}
       </Grid>
     </>
   );
@@ -76,19 +76,19 @@ const Quote = ({ quote, printRef }) => {
           data-testid="client"
           as="span"
           fontWeight="bold"
-          fontSize="1.2rem"
+          fontSize="1.1rem"
         >
           客戶： {quote.name}
         </Typography>
-        <Typography as="span" fontWeight="bold" fontSize="1.2rem">
+        <Typography as="span" fontWeight="bold" fontSize="1.1rem">
           地址：{quote.address}
         </Typography>
-        <Typography as="span" fontWeight="bold" fontSize="1.2rem">
+        <Typography as="span" fontWeight="bold" fontSize="1.1rem">
           日期：{date.toLocaleDateString()}
         </Typography>
       </Stack>
       <Divider sx={{ borderColor: "#000000", mt: 3 }} />
-      <Grid container rowSpacing={5} spacing={5} sx={{ mt: 2, mb: 5 }}>
+      <Grid container rowSpacing={3} spacing={2} sx={{ mt: 2, mb: 5 }}>
         <Grid item xs={8} sm={8} fontSize="large">
           工程項目：
         </Grid>
@@ -99,15 +99,15 @@ const Quote = ({ quote, printRef }) => {
           <Item content={content} index={index} key={index} />
         ))}
       </Grid>
-      <Divider sx={{ borderColor: "#000000", mt: 3 }} />
-      <Grid container rowSpacing={3} spacing={5} sx={{ pl: 15, mt: 2, mb: 5 }}>
+      <Divider sx={{ borderColor: "#000000", mt: 2 }} />
+      <Grid container rowSpacing={3} spacing={5} sx={{ pl: 15, mt: 2 }}>
         <Grid item xs={7} sm={8}></Grid>
         <Grid item xs={5} sm={4} fontSize="large" sx={{ letterSpacing: "1px" }}>
           <Box>總數:</Box>
           <Box mt={3}>${intlNum.format(totalPrice)}</Box>
         </Grid>
       </Grid>
-      <Grid container rowSpacing={3} spacing={5} sx={{ mt: 2, mb: 5 }}>
+      <Grid container rowSpacing={3} spacing={5} sx={{ mb: 5 }}>
         {quote.memo[0] !== "" && (
           <Grid item xs={12} sx={{ mt: 2 }} fontSize="large">
             工程備忘：
--- client/src/pages/MainPage/QuoteDetails.jsx
+++ client/src/pages/MainPage/QuoteDetails.jsx
@@ -1,5 +1,5 @@
 import { Box, Button, Container } from "@mui/material";
-import { useState, useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import quoteApi from "../../apis/quoteApi";
 import Loader from "../../components/Loader/Loader";
@@ -9,14 +9,13 @@ import { QuotesContext } from "../../context/quotesContext";
 import { UserContext } from "../../context/userContext";
 import { useNavigate } from "react-router-dom";
 import DownloadIcon from "@mui/icons-material/Download";
-import html2canvas from "html2canvas";
-import { jsPDF } from "jspdf";
+import { useReactToPrint } from "react-to-print";
 
 const QuoteDetails = () => {
   const { id } = useParams();
   const { selectedQuote, setSelectedQuote } = useContext(QuotesContext);
   const { userState } = useContext(UserContext);
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 400);
+
   let navigate = useNavigate();
 
   useEffect(() => {
@@ -39,25 +38,9 @@ const QuoteDetails = () => {
   // pdf ref
   const printRef = useRef(null);
 
-  const handleDownloadPDF = async (address) => {
-    const element = printRef.current;
-    const canvas = await html2canvas(element);
-    const data = canvas.toDataURL("image/png");
-
-    const pdf = new jsPDF();
-    const imgProperties = pdf.getImageProperties(data);
-    const pdfWidth = pdf.internal.pageSize.getWidth();
-    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
-
-    const xPosition = isMobile ? 50 : 0;
-    const pageWidth = isMobile ? pdfWidth * 0.6 : pdfWidth;
-    const pageHeight = isMobile ? pdfHeight * 0.4 : pdfHeight;
-    console.log(isMobile);
-    console.log(xPosition, pageWidth, pageHeight);
-
-    pdf.addImage(data, "PNG", xPosition, 0, pageWidth, pageHeight);
-    pdf.save(`${address}`);
-  };
+  const handlePrint = useReactToPrint({
+    content: () => printRef.current,
+  });
 
   return (
     <Container>
@@ -79,7 +62,7 @@ const QuoteDetails = () => {
             variant="contained"
             id="download-btn"
             color="primary"
-            onClick={() => handleDownloadPDF(selectedQuote.address)}
+            onClick={handlePrint}
             endIcon={<DownloadIcon />}
           >
             下載報價單
